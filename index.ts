2ef4e4a46f6ca003e82317a6aec24bd2e52fbb4c
Florian von Bock <f@vonbock.info>
2017-06-07 22:34:39 +0900

    initial commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+import express = require('express');
+let app = express();
+
+// For POST-Support
+let bodyParser = require('body-parser');
+let multer = require('multer');
+let upload = multer();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+app.get('/api/sayhello/:name', (request, response) => {
+    let name = request.params.name;
+
+    if (!isNaN(name)) {
+        response
+            .status(400)
+            .send('No string as name');
+    } else {
+        response.json({
+            "message": name
+        });
+    }
+});
+
+app.post('/api/sayhello', upload.array(), (request, response) => {
+    let name = request.body.name;
+
+    if (!isNaN(name)) {
+        response
+            .status(400)
+            .send('No string as name');
+    } else {
+        console.log('Hello ' + name);
+    }
+
+    response.send('POST request to homepage');
+});
+
+app.listen(3000);
